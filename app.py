081eb5f80d2b8876a316edfab55f27692fead7a0
aahan <aahannayak86@gmail.com>
2026-04-02 01:21:20 +0530

    file error debugged

--- app.py
+++ app.py
@@ -18,13 +18,18 @@ def add_task():
 	return jsonify({"error": "No Task Provided"}), 400
 @app.route('/tasks', methods=['DELETE'])
 def delete_task():
-	data=request.json
-	task=data.get("task")
-	if task:
-		removed_count=db.lrem("my_tasks, 0, task)
-	if removed_count>0:
-		return jsonify({"message": f"Deleted task: {task}"), 200
-	return jsonify({"error": "Task not found"}), 404
-return jsonify({"error: "No task provided"}), 400
+    data = request.json
+    task = data.get("task")
+    
+    if task:
+        # LREM removes the task from the list
+        # The '0' means "remove all occurrences of this task"
+        removed_count = db.lrem("my_tasks", 0, task)
+        
+        if removed_count > 0:
+            return jsonify({"message": f"Deleted task: {task}"}), 200
+        return jsonify({"error": "Task not found"}), 404
+        
+    return jsonify({"error": "No task provided"}), 400
 if __name__=="__main__":
 	app.run(host="0.0.0.0", port=5000)
